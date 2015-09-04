304e86e4df87f5a9c68a566689c0f27cbd003d58
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-04 22:19:48 +0700

    Update Main.cpp
    Nama : Muhammad Wildan Iskandar
    NIM : 1301144316

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
+#include "Clock_.h"
+/* This Program is Created By Muhammad Wildan Iskandar*/
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
-  return 0;
+    clock j1,j2,j3;
+    hour jam;
+    minute menit;
+    second detik;
+
+    j1 = MakeClock(2,30,4);
+    j2 = MakeClock(6,0,0);
+    jam = GetHour(j1);
+    menit = GetMinute(j2);
+    detik = GetSecond(j1);
+    cout << jam.jam<<endl;
+    cout<< menit.menit<<endl;
+    cout << detik.detik<<endl;
+    j3 = AddClock(j1,j2);
+    Print(j3);
+    cout << IsEqual(j1,j2);
+    cout<<endl;
+
+    SetHour(&j1,4);
+    SetMinute(&j1,8);
+    SetSecond(&j1,10);
+    jam = GetHour(j1);
+    menit = GetMinute(j1);
+    detik = GetSecond(j1);
+    cout << jam.jam<<endl;
+    cout<< menit.menit<<endl;
+    cout << detik.detik<<endl;
+    j3 = AddClock(j1,j2);
+    Print(j3);
+    cout << IsEqual(j1,j2);
+    getch();
+    return 0;
 }
