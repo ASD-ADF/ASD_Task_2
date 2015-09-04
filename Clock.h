f5ecf273206d9eaf34cfad786ca1c14eec6716ad
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-04 22:20:55 +0700

    Clock_.h
    Nama : Muhammad Wildan Iskandar
    NIM : 1301144316

--- Clock.h
+++ Clock.h
@@ -1,6 +1,40 @@
-#ifndef CLOCK_H_INCLUDED
-#define CLOCK_H_INCLUDED
+#ifndef CLOCK__H_INCLUDED
+#define CLOCK__H_INCLUDED
+/* This Program is Created By Muhammad Wildan Iskandar*/
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct hour {
+    int jam;
+};
+struct minute {
+    int menit;
+};
+struct second {
+    int detik;
+};
+struct clock {
+    hour hh;
+    minute mm;
+    second ss;
+};
+bool isValid (int hh, int mm, int ss);
+
+clock MakeClock (int hh, int mm, int ss);
+
+hour GetHour (clock c);
+minute GetMinute (clock c);
+second GetSecond (clock c);
+
+void SetHour (clock *c, int newhh);
+void SetMinute (clock *c, int newmm);
+void SetSecond (clock *c, int newss);
+
+bool IsEqual (clock c1, clock c2);
+clock AddClock (clock c1, clock c2);
+void Print (clock c);
 
 
-
-#endif // CLOCK_H_INCLUDED
+#endif // CLOCK__H_INCLUDED
