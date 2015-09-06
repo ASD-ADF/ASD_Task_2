289fae6132ec1da0b0d7f6aa3863c5ef73539a83
ariadrian <ariadrian.i@gmail.com>
2015-09-06 16:03:53 +0700

    clock yg c3 tidak bisa dijalankan

--- Clock.cpp
+++ Clock.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "Clock.h"
 
+using namespace std;
+
+int main()
+{
+    clock c1 = MakeClock(2,30,4);
+    clock c2 = MakeClock(6,0,0);
+    clock c3 = MakeClock(1,62,30);//tidak bisa dibuat karna isvalid mereturn false
+
+    PrintClock(c1);
+    PrintClock(c2);
+
+    cout << (GetHour(c1));
+    cout << (GetSecond(c2));
+
+    c3 = AddClock(c1,c2);
+
+    cout << (IsEqual(c1,c2));
+}
