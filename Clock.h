56fd89bfbb3f8a60f4d40c0bbd645889ce2403e3
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2015-09-07 23:51:24 +0700

    Rizkiyana Prima Putra (2)
    1301140181

--- Clock.h
+++ Clock.h
@@ -1,6 +1,42 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+#include <iostream>
+using namespace std;
 
+struct HOUR
+{
+    int jam;
+};
+struct MINUTE
+{
+    int menit;
+};
+struct SECOND
+{
+    int detik;
+};
+struct TIME
+{
+    HOUR HH;
+    MINUTE MM;
+    SECOND SS;
+};
+
+bool IsValid (int HH, int MM, int SS);
+
+TIME MakeClock (int HH, int MM, int SS);
+
+HOUR GetHour (TIME a);
+MINUTE GetMinute (TIME a);
+SECOND GetSecond (TIME a);
+
+void SetHour (TIME a, int newHH);
+void SetMinute (TIME a, int newMM);
+void SetSecond (TIME a, int newSS);
+
+bool IsEqual (TIME a1, TIME a2);
+TIME AddClock (TIME a1, TIME a2);
+void PrintClock (TIME a);
 
 #endif // CLOCK_H_INCLUDED
