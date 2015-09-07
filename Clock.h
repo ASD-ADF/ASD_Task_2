08f6ffa2ce4ee4d3aef2fe1d1093471f9e7527bf
itegarb <itbudiyarto@gmail.com>
2015-09-07 19:41:59 +0700

    1301144216clock.h

--- Clock.h
+++ Clock.h
@@ -1,6 +1,41 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+typedef int hour;
+typedef int minute;
+typedef int second;
+
+struct clock
+{
+    hour hh;
+    minute mm;
+    second ss;
+};
+
+//validator
+bool IsValid (int hh, int mm, int ss);
+
+//constructor
+clock MakeClock(int hh, int mm, int ss);
+
+//selector
+hour GetHour(clock c);
+minute GetMinute (clock c);
+second GetSecond (clock c);
+
+//value changer
+void SetHour (clock c, int newHH);
+void SetMinute(clock c, int newMM);
+void SetSecond(clock c, int newSS);
+
+//relational operation
+bool IsEqual (clock c1, clock c2);
+
+//arithmetic operation
+clock AddClock (clock c1, clock c2);
+
+//output proccess
+void PrintClock (clock c);
 
 
 #endif // CLOCK_H_INCLUDED
