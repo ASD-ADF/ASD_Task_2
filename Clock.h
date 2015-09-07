fbe573513911210eb5a434e1b4b40b5c6ae6dad2
febbyfeysyah <febbyfeysyah@gmail.com>
2015-09-07 22:21:11 +0700

    Clock ADT Header
    Nama : Febby Febriansyah
    Kelas : IF-38-01
    NIM   : 1301140371

--- Clock.h
+++ Clock.h
@@ -1,6 +1,33 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+typedef int Hour;
+typedef int Minute;
+typedef int Second;
+
+struct clock {
+    Hour HH;
+    Minute MM;
+    Second SS;
+};
+bool IsValid (int HH,int MM,int SS); //mengembalikan nilai true jika 0≤HH≤23, dan 0≤MM≤59, dan 0≤MM≤59
+clock MakeClock (int HH,int MM,int SS); //mengembalikan jam yang telah dibuat dari inputan
+
+//Selector function
+Hour GetHour (clock c);
+Minute GetMinute (clock c);
+Second GetSecond (clock c);
+
+//Value changer
+void SetHour (clock *c,int newHH);
+void SetMinute (clock *c,int newMM);
+void SetSecond (clock *c,int newSS);
+
+//Other Operation
+bool IsEqual (clock c1,clock c2);
+clock AddClock (clock c1,clock c2);
+void PrintClock (clock c);
+
 
 
 #endif // CLOCK_H_INCLUDED
