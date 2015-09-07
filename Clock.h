168c19bf20aeac73f837d8d1b0c88b7b39df8ef0
Annisa Rizki Pratiwi Wibowo <annisaarizki@yahoo.co.id>
2015-09-08 00:44:21 +0800

    annisarizki

--- Clock.h
+++ Clock.h
@@ -1,6 +1,36 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+typedef int Hour;
+typedef int Minute;
+typedef int Second;
+
+struct clock
+{
+    Hour HH;
+    Minute MM;
+    Second SS;
+};
+
+bool IsValid(int HH, int MM, int SS);
+
+clock MakeClock(int HH, int MM, int SS);
+
+Hour GetHour(clock c);
+Minute GetMinute(clock c);
+Second GetSecond (clock c);
+
+void SetHour(clock c, int newHH);
+void SetMinute(clock c, int newMM);
+void SetSecond(clock c, int newSS);
+
+bool IsEqual(clock c1, clock c2);
+clock AddClock(clock c1, clock c2);
+void PrintClock(clock c);
 
 
 #endif // CLOCK_H_INCLUDED
+
+//Annisa Rizki Pratiwi Wibowo
+//1301144391
+//IF-38-01
