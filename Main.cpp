3747b44ca5e1275c295a6a3bb0a3fbc15c6f9c28
mfarin <mfarinakhsanta@gmail.com>
2015-09-07 23:18:00 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     clock c1 = MakeClock(2,30,4)
-clock c2 = MakeClock(6,0,0)
-clock c3 = MakeClock(1,62,30) //should not be created as the input is invalid, use IsValid function
+clock c2 = MakeClock(3,0,0)
+clock c3 = MakeClock(1,40,30) //should not be created as the input is invalid, use IsValid function
 
 PrintClock(c1)
 PrintClock(c2)
