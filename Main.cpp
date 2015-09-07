12f4a302aded4eec77f2be692b41f972e156f743
rizalnazar <rizalnazar08@gmail.com>
2015-09-07 22:11:04 +0700

    Update Main.cpp
    rizal mochamad nazar
    1301144006

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
-
-using namespace std;
+#include "clock.h"
 
 int main()
 {
-  return 0;
+    clock itujam1,itujam2,itujambaru;
+    jam x;
+    menit y;
+    bool z;
+
+
+    itujam1=Buatjam(20,40,40);
+    itujam2=Buatjam(21,50,30);
+    x=ambiljam(itujam1);
+    y=ambilmenit(itujam2);
+    itujambaru=tambahjam(itujam1,itujam2);
+    z=sama(itujam1,itujam2);
+    cout<<z;
 }
+
