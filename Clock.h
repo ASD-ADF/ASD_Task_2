79e1a29344263e79b11c523e1067e85d9c452c09
Fauzan Rambang <fauzanrambang40@gmail.com>
2015-09-07 22:09:34 +0700

    Update Clock.h

--- Clock.h
+++ Clock.h
@@ -1,6 +1,21 @@
-#ifndef CLOCK_H_INCLUDED
-#define CLOCK_H_INCLUDED
+#ifndef CLOCK_INCLUDED
+#define CLOCK_INCLUDED
 
+struct clock
+{
+    int hour;
+    int minute;
+    int second;
+};
 
-
-#endif // CLOCK_H_INCLUDED
+bool IsValid(int hh,int mm, int ss);
+clock MakeClock(int h,int m, int s);
+int GetHour(clock jam);
+int GetMinute(clock menit);
+int GetSecond(clock detik);
+void SetHour(int jaman);
+void SetMinute(int menitan);
+void SetSecond(int detikan);
+bool IsEqual(clock waktu,clock waktu1);
+void PrintClock(clock pukul);
+#endif // CLOCK_INCLUDED
