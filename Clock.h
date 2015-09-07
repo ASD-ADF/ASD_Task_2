f472c91e57249bd0955e2a907124db35a449aafb
bakmandi <explodingc4@gmail.com>
2015-09-07 20:43:49 +0700

    Update Clock.h

--- Clock.h
+++ Clock.h
@@ -1,6 +1,36 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+struct hour{
+    int ho;};
+struct minute{
+    int mi;};
+struct second{
+    int se;};
+int newhh;
+int newmm;
+int newss;
 
+
+struct clock{
+    hour hh;
+    minute mm;
+    second ss;
+};
+
+bool isvalid (int hh, int mm, int ss);
+clock makeclock (int hh, int mm, int ss);
+
+hour gethour(clock c);
+minute getminute(clock c);
+second getsecond(clock c);
+
+void sethour (clock c, int newhh);
+void setminute (clock c, int newmm);
+void setsecond (clock c, int newss);
+
+bool isequal (clock c1, clock c2);
+clock addclock (clock c1, clock c2);
+void printclock (clock c);
 
 #endif // CLOCK_H_INCLUDED
