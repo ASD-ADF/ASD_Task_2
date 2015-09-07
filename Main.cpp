437e5e7f130d43e1e2ea4b77e5dd4d0d9b55bd09
aliyanurr <aliyanurr@gmail.com>
2015-09-07 22:16:23 +0700

    Update Main.cpp
    Aliya Nur R (1301144161)

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "clock.h"
 
 using namespace std;
 
 int main()
 {
-  return 0;
+    Clock c1 = MakeClock(4, 15, 55);
+    cout<<endl;
+    Clock c2 = MakeClock(2,63,10);
+    cout<<endl;
+    Clock c3 = MakeClock(1,56,0);
+    cout<<endl;
+    PrintClock(c1);
+    cout<<endl;
+    PrintClock(c2);
+    cout<<endl;
+    cout<<GetHour(c1)<<endl;
+    cout<<GetHour(c2)<<endl;
+    cout<<endl;
+    c3 = AddClock(c1, c2);
+    cout<<endl;
+    PrintClock(c3);
+    cout<<endl;
+    cout<<endl;
+    cout<<IsEqual(c1,c2);
+    cout<<endl;
+    return 0;
 }
+
