28015be3bc2a076204edd44b57fc2cd24d8fe417
rannyrpa <rannyputriamelia@gmail.com>
2015-09-07 23:26:53 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include "clock.h"
+
 
 using namespace std;
+bool valid;
+clock mm,mn;
+bool k;
 
-int main()
+int main ()
 {
-  return 0;
+    return 0;
+    valid =false;
+    valid=isvalid (07,10,06);
+    if (valid==true)
+    {
+        cout<<"jam sesuai\n";
+    }
+    else if (valid==false)
+
+    {
+        cout<<"jam tidak sesuai\n";
 }
+mm=makeclock (07,10,06);
+cout<<"jam sekarang adalah "<<mm.jam<" : "<<mm.menit<< " : "<<mm.detik<<"\n";
+cout<< "jam : ["<<gethour(mm)<<"]"<<"\n";
+cout<< "menit : ["<<getminute(mm)<<"]"<<"\n";
+cout<< "detik : ["<<getsecond(mm)<<"]"<<"\n";
+
