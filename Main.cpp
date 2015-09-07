bf037622dbb081be67baa7ae74065f4a36bf1c3a
Thoriq Abdul Aziz <riq.aziz@gmail.com>
2015-09-07 23:55:56 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,32 @@
+/*  Nama  : Thoriq Abdul Aziz M
+    Kelas : IF-38-01
+    Nim   : 1301144341 */
 #include <iostream>
-
+#include "clock.h"
+#include "clock.cpp"
 using namespace std;
 
 int main()
 {
-  return 0;
+    clock c1= MakeClock(3,3,4);
+    clock c2=MakeClock(6,0,0);
+    clock c3=MakeClock(1,62,30);
+    IsValid(1,62,30);
+
+    SetHour(&c1,2);
+    SetMinute(&c1,30);
+    SetSecond(&c1,04);
+
+    printclock(c1);
+    PrintClock(c2);
+
+    GetHour(c1);
+    GetMinute(c1);
+    GetSecond(c1);
+
+    c3=AddClock(c1,c2);
+
+    printclock(c3);
+    cout<<(IsEqual(c1,c2));
+    return 0;
 }
