70364ba5af3b5298eeaa71ce3355f6bb70cab98d
dessydkn <desst_dkn@yahoo.com>
2015-09-07 22:34:34 +0700

    Update Clock.h
    Dessy koesumaningrum
    IF3806
    1301144296

--- Clock.h
+++ Clock.h
@@ -1,6 +1,25 @@
-#ifndef CLOCK_H_INCLUDED
-#define CLOCK_H_INCLUDED
+#ifndef JAM_H_INCLUDED
+#define JAM_H_INCLUDED
+#include <iostream>
+
+struct clock
+{
+    int jam;
+    int menit;
+    int detik;
+};
+
+bool validasi(int jam, int menit, int detik);
+clock buatjam(int jam, int menit, int detik);
+
+int tampiljam(clock jl);
+int tampilmenit(clock jl);
+int tampildetik(clock jl);
+void aturjam(int jam);
+void aturmenit(int menit);
+void aturdetik(int detik);
+bool perbandingan(clock j1, clock j2);
+void jamkeluar(clock j1);
 
 
-
-#endif // CLOCK_H_INCLUDED
+#endif // JAM_H_INCLUDED
