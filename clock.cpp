1f34d9c79faf65f9699de49163d9cc72cc5f530f
fachriul <fachriul@gmail.com>
2015-09-07 23:07:10 +0700

    Create clock.cpp

---
+++ clock.cpp
@@ -0,0 +1,40 @@
+//Fachri Ul Albab
+//IF-38-01
+//1301140151
+
+#include "Clock.h"
+
+bool IsValid(int HH,MM,SS)
+{
+    if(((HH>=0)and(HH<=23))and((MM>=0)and(MM<=59))and((SS>=0)and(SS<=59))) {return true;}
+    else
+        {
+            return false;
+        }
+}
+
+clock CreateClock(int HH,MM,SS)
+{
+    clock cl;
+    cl.HH = HH;
+    cl.MM = MM;
+    cl.SS = SS;
+    return cl;
+}
+
+hour GetHour(clock c) { return c.HH; }
+minute GetMinute(clock c) { return c.MM; }
+second GetSecond(clock c) { return c.SS; }
+
+clock AddClock (clock c1, clock c2)
+{
+    clock c3;
+    int tm = 0; int th = 0;
+    if (c1.SS+c2.SS >= 60) { c3.SS = c1.SS + c2.SS - 60; tm = 1; }
+    else { c3.SS = c1.SS + c2.SS; }
+    if (c1.MM+c2.MM + tm >= 60) { c3.MM = c1.MM + c2.MM - 60 + tm; th = 1;}
+    else { c3.MM = c1.MM + c2.MM + tm; }
+    if (c1.HH+c2.HH + th >= 24) { c3.HH = c1.HH + c2.HH - 24 ;}
+    else { c3.HH = c1.HH + c2.HH + th; }
+    return c3;
+}
