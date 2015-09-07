afeeb7a49dee3799b90d805fef6717bfc3ce49c2
wahyulintang <wlinntang@gmail.com>
2015-09-07 22:28:13 +0700

    Clock
    Nama :  Wahyu Lintang S
    NIM : 1301144376
    Kelas :  IF-38-06

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include "Clock.h"
+#include "clock.cpp"
 
 using namespace std;
 
+
+Clock ca,cb,cx;
 int main()
 {
-  return 0;
+    Clock ca = MakeClock(2,30,4);
+    Clock cb = MakeClock(6,0,0);
+    Clock cx = MakeClock(1,62,30); //should not be created as the input is invalid, use IsValid function
+
+    PrintClock(ca);
+    PrintClock(cb);
+
+    cout << GetHour(ca) << endl;
+    cout << GetSecond(cb) << endl;
+
+    cx = AddClock (ca, cb);
+    PrintClock (cx);
+
+    cout << IsEqual(ca,cb);
+    cin.get();
+    return 0;
 }
