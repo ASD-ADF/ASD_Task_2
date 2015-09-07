a788703760b5d1c38ac4b048b677d40ca96961c4
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2015-09-07 23:52:12 +0700

    Rizkiyana Prima Putra (3)
    1301140181

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-
+#include "Clock.h"
 using namespace std;
 
 int main()
 {
+    TIME a1 = MakeClock(2,30,4);
+    cout<<endl;
+    TIME a2 = MakeClock(6,0,0);
+    cout<<endl;
+    TIME a3 = MakeClock(1,62,30);
+    cout<<endl;
+    cout<<"======================"<<endl;
+    PrintClock(a1);
+    cout<<endl;
+    PrintClock(a2);
+    cout<<endl;
+    cout<<"======================"<<endl;
+    cout<<GetHour(a1)<<endl;
+    cout<<GetSecond(a1)<<endl;
+    cout<<"======================"<<endl;
+    a3 = AddClock(a1,a2);
+    PrintClock(a3);
+    cout<<endl;
+    cout<<"======================"<<endl;
+    cout<<IsEqual(a1,a2)<<endl;
+
   return 0;
 }
