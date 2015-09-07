a0bfa0eb043a4a38f6e7d3e692094eb881a6e31f
zulalfa <zulalfa15@gmail.com>
2015-09-07 21:27:19 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
 using namespace std;
-
+int jam1,jam2,menit1,menit2,detik1,detik2,totaljam,totalmenit,totaldetik;
 int main()
 {
-  return 0;
+    detik1==0;
+    detik2==0;
+    do
+    {
+        cout<<"Masukkan Durasi Pertama : "<<endl;
+        cout<<"Jam : ";
+        cin>>jam1;
+        cout<<"Menit : ";
+        cin>>menit1;
+        cout<<"Detik : ";
+        cin>> detik1;
+        cout<<"Masukkan Durasi Kedua: "<<endl;
+        cout<<"Jam : ";
+        cin>>jam2;
+        cout<<"Menit : ";
+        cin>>menit2;
+        cout<<"Detik : ";
+        cin>> detik2;
+    totaldetik=detik1+detik2;
+    totalmenit=menit1+menit2;
+    totaljam=jam1+jam2;
+    }
+    while ((detik1>60) or (detik2>60));
+    if (totaldetik>60){
+        totaldetik=totaldetik-60;
+    ++totalmenit;}
+    else {totaldetik=totaldetik;}
+    if (totalmenit>60);
+
+    else {totalmenit=totalmenit;}
+    cout<<"total durasi: "<<totaljam<<"jam"<<totalmenit<<"menit"<<totaldetik;
+    return 0;
 }
