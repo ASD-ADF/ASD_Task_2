76961ee6d9138aff80466aad785ca5c1fa4a5e9c
eddomirsyad <irsyad.mbah@gmail.com>
2015-09-07 22:16:17 +0700

    main.cpp
    Eddo Muhammad Irsyad - 1301144266

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
-
+#include "jam.h"
 using namespace std;
 
 int main()
 {
-  return 0;
+    clock jam1, jam2, jam3;
+    jam a; menit b;bool c;
+
+    jam1=MakeClock(17,30,02);
+    jam2=MakeClock(04,20,30);
+    a=GetHour(jam1);
+    b=GetMinute(jam2);
+    jam3=AddClock(jam1, jam2);
+    c=IsEqual(jam1, jam2);
+    printClock(jam3);
+    cout<<c;
 }
