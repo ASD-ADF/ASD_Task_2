0cb23451fe12df2d8d50400457880948bf0271e8
BluezChrist <ultrarodney@gmail.com>
2015-09-07 23:41:28 +0700

    1301140236 S.Christian Adi P. clock.h

--- Clock.h
+++ Clock.h
@@ -1,6 +1,20 @@
 #ifndef CLOCK_H_INCLUDED
 #define CLOCK_H_INCLUDED
 
+struct clock {
+    int hh;
+    int mm;
+    int ss;
+};
+clock c1;
+clock c2;
 
+bool IsValid(int hh,int mm,int ss);
+int MakeClock(int hh,int mm,int ss);
+int GetHour(int hh);
+int GetMinute(int mm);
+int GetSecond(int ss);
+int AddClock(int hh);
+}
 
 #endif // CLOCK_H_INCLUDED
