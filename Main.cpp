fb865ac9ec440f1f7f1fb277fe7a7bb69b754db1
mfarin <mfarinakhsanta@gmail.com>
2015-09-07 23:17:09 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+# include "clock.h"
 
 using namespace std;
 
 int main()
 {
-  return 0;
+    clock c1 = MakeClock(2,30,4)
+clock c2 = MakeClock(6,0,0)
+clock c3 = MakeClock(1,62,30) //should not be created as the input is invalid, use IsValid function
+
+PrintClock(c1)
+PrintClock(c2)
+
+output(getHour(c1))
+output(getSecond(c2))
+
+c3 = AddClock(c1, c2)
+PrintClock(c3)
+
+output(IsEqual(c1,c2))
+    return 0;
 }
