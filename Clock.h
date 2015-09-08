646c378fa654ac4cb16ab6e4313ce038a2e9b041
Adam Geraldy <adamgeraldy@users.noreply.github.com>
2015-09-08 22:37:05 +0700

    First commit

--- Clock.h
+++ Clock.h
@@ -1,6 +1,32 @@
-#ifndef CLOCK_H_INCLUDED
-#define CLOCK_H_INCLUDED
+#ifndef CLOCK_H
+#define CLOCK_H
 
+#include <iostream>
 
+using namespace std;
 
-#endif // CLOCK_H_INCLUDED
+typedef int hour, minute, second;
+
+struct clock
+{
+    hour HH;
+    minute MM;
+    second SS;
+};
+
+bool isValid(hour h, minute m, second s);
+clock makeClock(hour h, minute m, second s);
+
+hour getHour(clock cl);
+minute getMinute(clock cl);
+second getSecond(clock cl);
+
+void setHour(clock cl, hour newHH);
+void setMinute(clock cl, minute newMM);
+void setSecond(clock cl, second newSS);
+
+bool isEqual(clock cl, clock cl2);
+clock addClock(clock cl, clock cl2);
+void printClock(clock cl);
+
+#endif // CLOCK_H
