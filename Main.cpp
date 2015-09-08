6579d4e8ec1d4c194b0c028b9a3fa0eca1b38087
diankusumasari <diankusumasari94@gmail.com>
2015-09-07 23:18:04 -0700

    Task 2 Clock
    Nama : Dian Kusumasari
    NIM   : 1103134317
    Kelas  : IF 38 06

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <conio.h>
+#include "Clock.h"
 
 using namespace std;
 
 int main()
 {
-  return 0;
+    cout << "This is true : \n";
+    cout << "\n";
+    clock c1 = MakeClock(2,30,4);
+    clock c2 = MakeClock(6,0,0);
+    clock c3 = MakeClock(1,62,30);
+
+    PrintClock(c1);
+    PrintClock(c2);
+
+    return GetHour(c1);
+    return GetSecond(c2);
+
+    c3 = AddClock(c1,c2);
+    PrintClock(c3);
+
+    return IsEqual(c1,c2);
+
+    return 0;
 }
