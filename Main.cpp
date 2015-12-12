b8666b9c26babd00f99c868e04a3756af7b86d1e
chandragk <cgilang742@gmail.com>
2015-12-12 18:26:25 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+using namespace std;
 using namespace std;
 
 int main()
-{
+int main()
+ {
+ {
   return 0;
+
+       cout<<" "<<endl;
+       return 0;
+       clock c1 = MakeClock(4,45,10);
+       clock c2 = MakeClock(7,30,20);
+       clock c3 = MakeClock(9,15,30);
+    displayClock(c1);
+        cout<<"\n";
+    displayClock(c2);
+        cout<<"\n";
+
+    sethour(&c1,5);
+    setminute(&c1,10);
+    setsecond(&c1,30);
+
+    gethour(c1);
+    getminute(c1);
+    getsecond(c1);
+
+    c3 = AddClock(c1,c2);
+
+    displayClock(c3);
+    cout<<"\n";
+
+    cout<<IsEqual(c1,c2)<<"\n";
+    cout<<gethour(c1);
+    return 0;
+
+
+getch ();
+return 0;
 }
+ }
