a57b5984b24aeea8f884260369a9b071bae105a1
Aziza <azizahp.27@gmail.com>
2016-09-12 21:31:10 +0700

    Aziza Hayupratiwi 1301150440

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 39-06
+    KELOMPOK  : 8
+    NAMA(NIM) : [Aziza Hayupratiwi][1301150440] , [M. Luthfi A.S][1301154328] , [Rafi][nim] , [nama][nim]
 **/
 
 
@@ -16,31 +16,39 @@ int tgl, bln, thn;
 
 
 int main() {
-    cout << "Hello!" << endl;
-    cout << "masukkan nama orang 1: ";
-    cin>>nama;
-    cout << "masukkan tanggal lahir orang 1: ";
-    cin>>tgl;
-    cout << "masukkan bulan lahir orang 1: ";
-    cin>>bln;
-    cout << "masukkan tahun lahir orang 1: ";
-    cin>>thn;
+    do {
+        cout << "Hello!" << endl;
+        cout << "Masukkan Nama Orang 1: ";
+        cin>>nama;
+        cout << "Masukkan Tanggal Lahir Orang 1: ";
+        cin>>tgl;
+        cout << "Masukkan Bulan Lahir Orang 1: ";
+        cin>>bln;
+        cout << "Masukkan Tahun Lahir Orang 1: ";
+        cin>>thn;
+        cout << endl;
 
-    o1 = create_orang(nama, tgl,bln,thn);
-    show_orang(o1);
+        o1 = create_orang(nama, tgl,bln,thn);
+        show_orang(o1);
 
-    cout << "masukkan nama orang 2: ";
-    cin>>nama;
-    cout << "masukkan tanggal lahir orang 2: ";
-    cin>>tgl;
-    cout << "masukkan bulan lahir orang 2: ";
-    cin>>bln;
-    cout << "masukkan tahun lahir orang 2: ";
-    cin>>thn;
+        cout << "Masukkan Nama Orang 2: ";
+        cin>>nama;
+        cout << "Masukkan Tanggal Lahir Orang 2: ";
+        cin>>tgl;
+        cout << "Masukkan Bulan Lahir Orang 2: ";
+        cin>>bln;
+        cout << "Masukkan Tahun Lahir Orang 2: ";
+        cin>>thn;
+        cout << endl;
 
-    o2 = create_orang(nama, tgl,bln,thn);
-    show_orang(o2);
+        o2 = create_orang(nama, tgl,bln,thn);
+        show_orang(o2);
 
-    cout<< "selisih hari : "<<selisih_hari(o1.tgl_lahir,o2.tgl_lahir)<<" hari";
+        if (o1.tgl_lahir.yy != o2.tgl_lahir.yy) {
+            cout << "Tahun Lahir Harus Sama! Harap Masukkan Tahun Lahir Kembali";
+        } else {
+            cout<< "Selisih Hari : "<<selisih_hari(o1.tgl_lahir,o2.tgl_lahir)<<" hari" << endl;
+        }
+    } while (o1.tgl_lahir.yy != o2.tgl_lahir.yy);
     return 0;
 }
--- orang.cpp
+++ orang.cpp
@@ -1,7 +1,7 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 39-06
+    KELOMPOK  : 8
+    NAMA(NIM) : [Aziza Hayupratiwi][1301150440] , [M. Luthfi A.S][1301154328] , [Rafi][nim] , [nama][nim]
 **/
 
 
@@ -13,10 +13,12 @@ orang create_orang(string nama, int tgl, int bln, int thn) {
     * fungsi memanggil fungsi create_tanggal untuk membuat tanggal lahir
     **/
     orang org;
+    tanggal tgll;
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    org.nama = nama;
+    tgll = create_tanggal(tgl, bln, thn);
+    org.tgl_lahir = tgll;
     // YOUR CODE ENDS HERE
     //=================================================
     return org;
@@ -29,7 +31,7 @@ int hitung_umur(orang org) {
     int umur = 0;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    umur = 2016 - org.tgl_lahir.yy;
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -42,7 +44,38 @@ void show_orang(orang org) {
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    string ket_bln;
+    cout << "Nama: "<< org.nama << endl;
 
+    if (org.tgl_lahir.mm==1) {
+        ket_bln="Januari";
+    } else if (org.tgl_lahir.mm==2) {
+        ket_bln="Februari";
+    } else if (org.tgl_lahir.mm==3) {
+        ket_bln="Maret";
+    } else if (org.tgl_lahir.mm==4) {
+        ket_bln="April";
+    } else if (org.tgl_lahir.mm==5) {
+        ket_bln="Mei";
+    } else if (org.tgl_lahir.mm==6) {
+        ket_bln="Juni";
+    } else if (org.tgl_lahir.mm==7) {
+        ket_bln="Juli";
+    } else if (org.tgl_lahir.mm==8) {
+        ket_bln="Agustus";
+    } else if (org.tgl_lahir.mm==9) {
+        ket_bln="September";
+    } else if (org.tgl_lahir.mm==10) {
+        ket_bln="Oktober";
+    } else if (org.tgl_lahir.mm==11) {
+        ket_bln="November";
+    } else if (org.tgl_lahir.mm==12) {
+        ket_bln="Desember";
+    }
+
+    cout << "Tanggal Lahir: "<< org.tgl_lahir.dd <<" "<< ket_bln /**org.tgl_lahir.mm**/ <<" "<< org.tgl_lahir.yy << endl;
+    cout << "Umur: "<< hitung_umur(org) << endl;
+    cout << endl;
 
     // YOUR CODE ENDS HERE
     //=================================================
