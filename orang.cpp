4056a8c896b4697bb7e072f8392e853448ee977a
Kharisma Oktavianing Herwanda <kharisma.herwanda96@gmail.com>
2016-09-13 22:48:52 +0700

    Add files via upload
    Bismillah

--- orang.cpp
+++ orang.cpp
@@ -1,49 +1,52 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
-**/
+    KELAS     : IF-39-06
+    KELOMPOK  : 4
+    NAMA(NIM) : RAMA ADITYA MAULANA 1301150034, KHARISMA OKTAVIANING HERWANDA 1301154496, HUTOMO AJI 1301154384, RIZKI HIDAYATULLAH 1301154118
+ **/
 
+ #include "orang.h"
 
-#include "orang.h"
-
-orang create_orang(string nama, int tgl, int bln, int thn) {
+ orang create_orang(string nama, int tgl, int bln, int thn) {
     /**
-    * fungsi mengeset orang org dengan nama, tgl, bln, thn, dan mengembalikan hasilnya
-    * fungsi memanggil fungsi create_tanggal untuk membuat tanggal lahir
+    *fungsi mengeset orang org dengan nama, tgl, bln, thn, mengembalikan hasilnya
+    *fungsi memanggil fungsi create_tanggal untuk membuat tanggal lahir
     **/
     orang org;
-    //=================================================
-    // YOUR CODE STARTS HERE
+    //==================================================
+    //YOUR CODE STARTS HERE
+    org.nama = nama;
+    org.tgl_lahir.dd = tgl;
+    org.tgl_lahir.mm = bln;
+    org.tgl_lahir.yy = thn;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+    //YOUR CODE ENDS HERE
+    //==================================================
     return org;
 }
 
 int hitung_umur(orang org) {
     /**
-    * fungsi menghitung umur orang org berdasarkan tahun 2016
+    *fungsi menghitung umur orang org berdasarkan tahun 2016
     **/
     int umur = 0;
-    //=================================================
-    // YOUR CODE STARTS HERE
-
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+    //==================================================
+    //YOUR CODE ENDS HERE
+    umur=2016-org.tgl_lahir.yy;
     return umur;
 }
 
 void show_orang(orang org) {
     /**
-    * fungsi menampilkan nama, tanggal lahir dan umur orang org
+    *fungsi menampilkan nama, tanggal lahir dan umur orang org
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
+    //==================================================
+    //YOUR CODE STARTS HERE
+        cout<<"Nama : "<<org.nama<<endl;
+        cout<<"Tanggal Lahir : ";
+        show_tanggal(org.tgl_lahir);
+        cout<<endl;
+        cout<<"Umur : "<<hitung_umur(org)<<endl;;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+    //YOUR CODE ENDS HERE
+    //==================================================
 }
--- tanggal.cpp
+++ tanggal.cpp
@@ -1,82 +1,175 @@
-/**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
-**/
+#include"tanggal.h"
 
-#include <string>
-#include "tanggal.h"
-
-tanggal create_tanggal(int dd,int mm,int yy) {
+tanggal create_tanggal(int dd, int mm, int yy) {
     /**
-    * fungsi mengeset tanggal tgl dengan hari, bulan, dan tahun dan mengembalikan hasilnya
+    *fungsi mengeset tanggal tgl dengan hari, bulan, dan tahun dan mengembalikan hasilnya
     **/
 
     tanggal tgl;
-    //=================================================
-    // YOUR CODE STARTS HERE
+    //==================================================
+    //YOUR CODE STARTS HERE
 
+    tgl.dd = dd;
+    tgl.mm = mm;
+    tgl.yy = yy;
 
-    // YOUR CODE ENDS HERE
-    //=================================================
+    //YOUR CODE ENDS HERE
+    //==================================================
     return tgl;
 }
 
 bool cek_tanggal(tanggal tgl) {
     /**
-    * fungsi mengecek tanggal
-    * fungsi mengembalikan nilai true jika isi tanggal sesuai dengan bulannya
-    * fungsi mengembalikan nilai false jika isi tanggal atau bulan atau tahun salah
+    *fungsi mengecek tanggal
+    *fungsi mengembalikan nilai true jika isi tanggal sesuai dengan bulannya
+    *fungsi mengembalikan nilai false jika isi tanggal atau bulan atau tahun salah
     **/
+
     bool ok = true;
-    //=================================================
-    // YOUR CODE STARTS HERE
-
-
-    // YOUR CODE ENDS HERE
-    //=================================================
-    return ok;
+    int bulan = tgl.mm;
+    int tgl_sekarang = 0;
+    //==================================================
+    //YOUR CODE STARTS HERE
+        if (bulan != tgl_sekarang || tgl_sekarang == 100) {
+            ok != ok;
+        }
+    //YOUR CODE ENDS HERE
+    //==================================================
 }
 
 void show_tanggal(tanggal tgl) {
     /**
-    * fungsi menampilkan tanggal bulan tahun dengan format menampilkan nama bulan
-    * contoh : 1 Januari 1980
+    *fungsi menampilkan tanggal bulan tahun dengan format menampilkan nama bulan
+    *contoh : 1 Januari 1980
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
+    //==================================================
+    //YOUR CODE STARTS HERE
+    cout<<tgl.dd;
+    switch(tgl.mm)
+    {
+case 1 :
+    cout<<" Januari ";
+    break;
+case 2 :
+    cout<<" Februari ";
+    break;
+case 3 :
+    cout<<" Maret ";
+    break;
+case 4:
+    cout<<" April ";
+    break;
+case 5 :
+    cout<<" Mei ";
+    break;
+case 6 :
+    cout<<" Juni ";
+    break;
+case 7 :
+    cout<<" Juli ";
+    break;
+case 8 :
+    cout<<" Agustus ";
+    break;
+case 9 :
+    cout<<" September ";
+    break;
+case 10 :
+    cout<<" Oktober ";
+    break;
+case 11 :
+    cout<<" November ";
+    break;
+case 12 :
+    cout<<" Desember ";
+    break;
+default :
+    cout<<"Bulan tidak sesuai";
+    break;
+    }
+    cout<<tgl.yy;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+    //YOUR CODE ENDS HERE
+    //==================================================
 }
 
-void edit_tanggal(tanggal &tgl, int dd, int mm, int yy) {
+void edit_tanggal(tanggal tgl, int dd, int mm, int yy) {
     /**
-    * fungsi mengubah isi tanggal dari variabel tgl
+    *fungsi mengubah isi tanggal dari variabel tgl
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
+    //==================================================
+    //YOUR CODE STARTS HERE
+    cout<<"Tanggal sebelumnya : ";
+    show_tanggal(tgl);
+    cout<<"Masukkan tanggal baru : ";
+    cin>>dd;
+    cout<<"Masukkan bulan baru : ";
+    cin>>mm;
+    cout<<"Masukkan tahun baru : ";
+    cin>>yy;
 
+    tgl.dd = dd;
+    tgl.mm = mm;
+    tgl.yy = yy;
 
-    // YOUR CODE ENDS HERE
-    //=================================================
+    cout<<"Berhasil!!!, Tanggal telah berubah menjadi : ";
+    cout<<tgl.dd<<" "<<tgl.mm<<" "<<tgl.yy;
+
+    //YOUR CODE ENDS HERE
+    //==================================================
+
 }
 
-int selisih_hari(tanggal tgl1, tanggal tgl2) {
-    /**
-    * fungsi menghitung selisih hari dari variable tgl1 dan tgl2
-    * fungsi selalu mengembalikan selisih dalam nilai positif
-    * syarat tahun tgl1 dan tgl2 harus sama
-    * jika tahun pada tgl1 berbeda dengan tgl2, maka fungsi mengembalikan nilai -1
-    **/
-    int selisih = -1;
-    //=================================================
-    // YOUR CODE STARTS HERE
+int jumlah_hari_bulan(int bulan, int tahun) {
+    int jumlah_tgl = 0;
+    switch(bulan){
+case 1 :
+    jumlah_tgl = 31;
+    break;
+case 2 :
+    jumlah_tgl = 31;
+    break;
+case 3 :
+    if (tahun %4 == 0)
+    {
+        jumlah_tgl = 29;
+    }
+    else{
+        jumlah_tgl = 28;
+    }
+    break;
+case 4 :
+    jumlah_tgl = 31;
+    break;
+case 5 :
+    jumlah_tgl = 30;
+    break;
+case 6 :
+    jumlah_tgl = 31;
+    break;
+case 7 :
+    jumlah_tgl = 30;
+    break;
+case 8 :
+    jumlah_tgl = 31;
+    break;
+case 9 :
+    jumlah_tgl = 31;
+    break;
+case 10 :
+    jumlah_tgl = 30;
+    break;
+case 11 :
+    jumlah_tgl = 31;
+    break;
+case 12 :
+    jumlah_tgl = 0;
+    break;
+default :
+    jumlah_tgl = 100;
+    break;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
-    return selisih;
+    }
+    return jumlah_tgl;
 
 }
