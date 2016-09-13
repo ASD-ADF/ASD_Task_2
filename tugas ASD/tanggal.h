2745b522a0aaddca33feb41957e9d789e7db925c
Isabella Vichita Kacaribu <Bella05101997@gmail.com>
2016-09-13 11:27:54 -0700

    Add files via upload

---
+++ tugas ASD/tanggal.h
@@ -0,0 +1,29 @@
+k?/**
+    KELAS     :
+    KELOMPOK  :
+    NAMA(NIM) : [ISABELLA VICHITA KACARIBU][1301154637] , [AMIN AENURAHMAN][1301154398] , [ALFIN PRATAMA][NIM] , [HAFIZH FAUZANDI][1301154272]
+**/
+
+
+#ifndef TANGGAL_H_INCLUDED
+#define TANGGAL_H_INCLUDED
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct tanggal {
+    //=================================================
+    // YOUR CODE STARTS HERE
+    int tgl, bln, thn;
+    // YOUR CODE ENDS HERE
+    //=================================================
+};
+
+tanggal create_tanggal(int dd,int mm,int yy);
+bool cek_tanggal(tanggal tgl);
+void show_tanggal(tanggal tgl);
+void edit_tanggal(tanggal &tgl, int dd, int mm, int yy);
+int selisih_hari(tanggal tgl1, tanggal tgl2);
+
+#endif // TANGGAL_H_INCLUDED
