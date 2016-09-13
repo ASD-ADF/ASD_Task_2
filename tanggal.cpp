e6eec9eb7d57e30e2661e0b5f24bbbf88a5e3c82
rpebi18 <rianpebi@ymail.com>
2016-09-13 21:19:39 +0700

    Add files via upload

--- tanggal.cpp
+++ tanggal.cpp
@@ -1,7 +1,7 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 39-06
+    KELOMPOK  : 1
+    NAMA(NIM) : [Muhammad Iqbal][13401154300] , [Vatana Rianti Aldefi][1301154566] , [Ibrahim Bimo Wicaksono][1301154356] , [Rian Pebi][1301154426]
 **/
 
 #include <string>
@@ -15,6 +15,9 @@ tanggal create_tanggal(int dd,int mm,int yy) {
     tanggal tgl;
     //=================================================
     // YOUR CODE STARTS HERE
+    tgl.dd=dd;
+	tgl.mm=mm;
+	tgl.yy=yy;
 
 
     // YOUR CODE ENDS HERE
@@ -31,7 +34,15 @@ bool cek_tanggal(tanggal tgl) {
     bool ok = true;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    if (tgl.dd>=31 || tgl.dd<=0){
+    ok=false;
+    }
+    if (tgl.mm>=12 || tgl.dd<=0){
+    ok=false;
+    }
+    if (tgl.yy>=2016){
+    ok=false;
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -45,7 +56,47 @@ void show_tanggal(tanggal tgl) {
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    switch (tgl.mm)
+     {
+ case 1:
+    cout<<"Januari ";
+    break;
+ case 2:
+    cout<<"Februari ";
+    break;
+ case 3:
+    cout<<"Maret ";
+    break;
+ case 4:
+    cout<<"April ";
+    break;
+ case 5:
+    cout<<"Mei ";
+    break;
+ case 6:
+    cout<<"Juni ";
+    break;
+ case 7:
+    cout<<"Juli ";
+    break;
+ case 8:
+    cout<<"Agustus ";
+    break;
+ case 9:
+    cout<<"September ";
+    break;
+ case 10:
+    cout<<"Oktober ";
+    break;
+ case 11:
+    cout<<"November ";
+    break;
+ case 12:
+    cout<<"Desember ";
+    break;
 
+
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -57,6 +108,12 @@ void edit_tanggal(tanggal &tgl, int dd, int mm, int yy) {
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    cout<<"masukkan tanggal yang benar";
+    cin>>tgl.dd;
+    cout<<"masukkan bukan yang benar";
+    cin>>tgl.mm;
+    cout<<"masukkan tahun yang benar";
+    cin>>tgl.yy;
 
 
     // YOUR CODE ENDS HERE
@@ -71,8 +128,21 @@ int selisih_hari(tanggal tgl1, tanggal tgl2) {
     * jika tahun pada tgl1 berbeda dengan tgl2, maka fungsi mengembalikan nilai -1
     **/
     int selisih = -1;
+    int n;
+    int s;
     //=================================================
     // YOUR CODE STARTS HERE
+    if (tgl1.yy==tgl2.yy){
+    s=tgl1.dd - tgl2.dd;
+    if (s<0){
+        s=s*-1;
+    }
+    }
+    n=tgl1.mm-tgl2.mm;
+    if (n<0){
+        n=n*-1;
+    }
+    selisih=s+n;
 
 
     // YOUR CODE ENDS HERE
