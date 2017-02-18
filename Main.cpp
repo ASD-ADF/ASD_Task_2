19ce0926a66c29055770b798bc655c7555fdea1f
Taufiq Akmal Dawami <akmaldawamitaufiq@gmail.com>
2017-02-18 19:32:16 +0700

    1301164426 - Task 2 - Selesai

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include "buku.h"
 #include "tanggal.h"
-#include "aplikasi.h"
+//#include "aplikasi.h"
 
 using namespace std;
 
@@ -21,7 +21,8 @@ void menu_kembali();
 void menu_tampil();
 
 
-int main() {
+int main()
+{
 
     /**
     * fungsi main hanya memanggil main_menu
@@ -35,15 +36,27 @@ int main() {
 }
 
 
-void main_menu() {
+void main_menu()
+{
     /**
     * fungsi untuk menampilkan main menu
     * dan menerima input pilihan menu dari user
     **/
     int pil;
-    do {
+    do
+    {
+        cout<<"Menu"<<endl;
+        cout<<" "<<endl;
+        cout<<"1. Masukkan Buku"<<endl;
+        cout<<"2. Tampil Buku"<<endl;
+        cout<<"3. Pinjam Buku"<<endl;
+        cout<<"4. Kembalikan Buku"<<endl;
+        cout<<"0. Exit"<<endl;
+        cout<<" "<<endl;
+        cout<<"Masukkan pilihan anda: "<<endl;
         cin>>pil;
-        switch(pil) {
+        switch(pil)
+        {
         case 1:
             menu_create_buku();
             break;
@@ -61,21 +74,28 @@ void main_menu() {
         default:
             break;
         }
-    } while pil!=0;
+    }
+    while (pil!=0);
 }
 
-void menu_create_buku() {
+void menu_create_buku()
+{
     /**
     * fungsi meminta input judul dan pengarang dari user
     * fungsi membuat buku baru dan menyimpannya ke dalam array daftar_buku
     **/
     string judul,pengarang;
+    cout << "\nMasukkan judul buku : "<<endl;
     cin>>judul;
+    cout<<" "<<endl;
+    cout << "Masukkan pengarang buku : "<<endl;
     cin>>pengarang;
     daftar_buku[jumlah_buku++] = create_buku(judul, pengarang);
+    cout<<" "<<endl;
 }
 
-void menu_pinjam() {
+void menu_pinjam()
+{
     /**
     * fungsi meminta input tanggal, bulan, tahun dari user
     * fungsi meminta input id buku berdasar urutan buku pada array daftar_buku
@@ -84,14 +104,21 @@ void menu_pinjam() {
     int tgl,bln,thn;
     int id_buku;
 
+
+    cout << "\nMasukkan Tanggal : "<<endl;
     cin>>tgl;
+    cout << "\nMasukkan Bulan : "<<endl;
     cin>>bln;
+    cout << "\nMasukkan Tahun : "<<endl;
     cin>>thn;
+    cout << "\nMasukkan ID Buku : "<<endl;
     cin>>id_buku;
     pinjam_buku(daftar_buku[id_buku],tgl,bln,thn);
+    cout<<" "<<endl;
 }
 
-void menu_kembali() {
+void menu_kembali()
+{
     /**
     * fungsi meminta input tanggal, bulan, tahun dari user
     * fungsi meminta input id buku berdasar urutan buku pada array daftar_buku
@@ -103,23 +130,31 @@ void menu_kembali() {
     int id_buku;
     int selisih;
 
+    cout << "\nMasukkan Tanggal : "<<endl;
     cin>>tgl;
+    cout << "\nMasukkan Bulan : "<<endl;
     cin>>bln;
+    cout << "\nMasukkan Tahun : "<<endl;
     cin>>thn;
+    cout << "\nMasukkan ID Buku : "<<endl;
     cin>>id_buku;
     selisih = kembalikan_buku(daftar_buku[id_buku],tgl,bln,thn);
-    cout<<selisih;
+    cout << "\n Durasi meminjam " << selisih << " Hari" << endl;
+    cout<<" "<<endl;
 }
 
-void menu_tampil() {
+void menu_tampil()
+{
     /**
     * fungsi menampilkan daftar buku yang ada di dalam array daftar_buku
     **/
     buku b;
-    for(int i=0; i<jumlah_buku; i++) {
-        cout<<i;
+    for(int i=0; i<jumlah_buku; i++)
+    {
+        cout<<"\nID Buku : " << i << endl;
         b = daftar_buku[i];
         tampil_buku(b);
         cout<<endl;
     }
+    cout<<" "<<endl;
 }
--- tanggal.cpp
+++ tanggal.cpp
@@ -1,5 +1,5 @@
 /**
-    KELAS     :
+    KELAS     : IF 40-02
     KELOMPOK  :
     NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
 **/
@@ -8,7 +8,8 @@
 #include "tanggal.h"
 
 
-tanggal create_tanggal(int dd,int mm,int yy) {
+tanggal create_tanggal(int dd,int mm,int yy)
+{
     /**
     * fungsi mengeset tanggal tgl dengan hari, bulan, dan tahun dan mengembalikan hasilnya
     **/
@@ -16,14 +17,16 @@ tanggal create_tanggal(int dd,int mm,int yy) {
     tanggal tgl;
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    tgl.dd = dd;
+    tgl.mm = mm;
+    tgl.yy = yy;
     // YOUR CODE ENDS HERE
     //=================================================
     return tgl;
 }
 
-bool cek_tanggal(tanggal tgl) {
+bool cek_tanggal(tanggal tgl)
+{
     /**
     * fungsi mengecek tanggal
     * fungsi mengembalikan nilai true jika isi tanggal sesuai dengan bulannya
@@ -32,27 +35,79 @@ bool cek_tanggal(tanggal tgl) {
     bool ok = true;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    if ((tgl.mm == 1) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 2) && (tgl.dd < 28))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 3) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 4) && (tgl.dd < 30))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 5) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 6) && (tgl.dd < 30))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 7) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 8) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 9) && (tgl.dd < 30))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 10) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 11) && (tgl.dd < 30))
+    {
+        ok = true;
+    }
+    else if ((tgl.mm == 12) && (tgl.dd < 31))
+    {
+        ok = true;
+    }
+    else
+    {
+        ok = false;
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
     return ok;
 }
 
-void tampil_tanggal(tanggal tgl) {
+void tampil_tanggal(tanggal tgl)
+{
     /**
     * fungsi menampilkan tanggal bulan tahun dengan format menampilkan nama bulan
     * contoh : 1 Januari 1980
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    cout << tgl.dd <<"/"<< tgl.mm <<"/"<< tgl.yy << endl;
     // YOUR CODE ENDS HERE
     //=================================================
 }
 
-int selisih_hari(tanggal tgl1, tanggal tgl2) {
+int selisih_hari(tanggal tgl1, tanggal tgl2)
+{
     /**
     * fungsi menghitung selisih hari dari variable tgl1 dan tgl2
     * fungsi selalu mengembalikan selisih dalam nilai positif
@@ -62,10 +117,11 @@ int selisih_hari(tanggal tgl1, tanggal tgl2) {
     int selisih = -1;
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    if (tgl1.yy == tgl2.yy)
+    {
+        selisih = tgl2.dd - tgl1.dd;
+    }
     // YOUR CODE ENDS HERE
     //=================================================
     return selisih;
-
 }
