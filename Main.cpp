f66a21fb3c1a833a415ad65d40af932396eab02d
Attaaa <hatta.eka.p@gmail.com>
2017-02-18 10:52:50 +0700

    Sudah Selesai

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,13 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 40 02
+    KELOMPOK  : 2
+    NAMA(NIM) : [Muh. Hatta Eka Putra][1301160444] , [Rifki Mifathur Sutomo][1301164225] , [Fahmi Hidayat][1301164312] , [Elsya Putri Sutomo][1301160201]
 **/
 
 #include <iostream>
 #include "buku.h"
 #include "tanggal.h"
-#include "aplikasi.h"
+//#include "aplikasi.h"
 
 using namespace std;
 
@@ -21,7 +21,8 @@ void menu_kembali();
 void menu_tampil();
 
 
-int main() {
+int main()
+{
 
     /**
     * fungsi main hanya memanggil main_menu
@@ -35,15 +36,27 @@ int main() {
 }
 
 
-void main_menu() {
+void main_menu()
+{
     /**
     * fungsi untuk menampilkan main menu
     * dan menerima input pilihan menu dari user
     **/
     int pil;
-    do {
+
+    do
+    {
+        cout <<"   MENU "<<endl<<endl;
+        cout <<" 1. Masukkan Buku"<<endl;
+        cout <<" 2. Tampil Buku"<<endl;
+        cout <<" 3. Pinjam Buku"<<endl;
+        cout <<" 4. Kembalikan Buku"<<endl;
+        cout <<" 0. Keluar"<<endl<<endl;
+        cout <<" > Pilih : ";
         cin>>pil;
-        switch(pil) {
+        cout<<endl;
+        switch(pil)
+        {
         case 1:
             menu_create_buku();
             break;
@@ -58,24 +71,33 @@ void main_menu() {
             break;
         case 0:
             break;
-        default:
-            break;
         }
-    } while pil!=0;
+    }
+    while (pil!=0);
 }
 
-void menu_create_buku() {
+void menu_create_buku()
+{
     /**
     * fungsi meminta input judul dan pengarang dari user
     * fungsi membuat buku baru dan menyimpannya ke dalam array daftar_buku
     **/
     string judul,pengarang;
-    cin>>judul;
-    cin>>pengarang;
+
+    cout <<endl;
+    cout <<" > Judul Buku : ";
+    cin >>judul;
+    cout <<endl;
+    cout <<" > Nama Pengarang : ";
+    cin >>pengarang;
+    cout <<endl;
     daftar_buku[jumlah_buku++] = create_buku(judul, pengarang);
+
+    main_menu();
 }
 
-void menu_pinjam() {
+void menu_pinjam()
+{
     /**
     * fungsi meminta input tanggal, bulan, tahun dari user
     * fungsi meminta input id buku berdasar urutan buku pada array daftar_buku
@@ -84,14 +106,28 @@ void menu_pinjam() {
     int tgl,bln,thn;
     int id_buku;
 
+    cout <<" > Tanggal : ";
     cin>>tgl;
+    cout <<endl;
+    cout <<" > Bulan : ";
     cin>>bln;
+    cout <<endl;
+    cout <<" > Tahun : ";
     cin>>thn;
+    cout <<endl;
+    cout <<" > Id Buku : ";
     cin>>id_buku;
+    cout <<endl;
+
+
     pinjam_buku(daftar_buku[id_buku],tgl,bln,thn);
+
+
+    main_menu();
 }
 
-void menu_kembali() {
+void menu_kembali()
+{
     /**
     * fungsi meminta input tanggal, bulan, tahun dari user
     * fungsi meminta input id buku berdasar urutan buku pada array daftar_buku
@@ -103,23 +139,41 @@ void menu_kembali() {
     int id_buku;
     int selisih;
 
+    cout <<" > Tanggal : ";
     cin>>tgl;
+    cout <<endl;
+    cout <<" > Bulan : ";
     cin>>bln;
+    cout <<endl;
+    cout <<" > Tahun : ";
     cin>>thn;
+    cout <<endl;
+    cout <<" > Id Buku : ";
     cin>>id_buku;
     selisih = kembalikan_buku(daftar_buku[id_buku],tgl,bln,thn);
-    cout<<selisih;
+    if (selisih != -1)
+    {
+        cout<<" > Lama Peminjaman : "<<selisih<<endl<<endl;
+    }
+    else
+    {
+        cout<<" > Mohon Maaf, Tanggal Yang Anda Masukkan Salah"<<endl<<endl;;
+    }
+    main_menu();
 }
 
-void menu_tampil() {
+void menu_tampil()
+{
     /**
     * fungsi menampilkan daftar buku yang ada di dalam array daftar_buku
     **/
     buku b;
-    for(int i=0; i<jumlah_buku; i++) {
-        cout<<i;
+    for(int i=0; i<jumlah_buku; i++)
+    {
+        cout<<"  > Id Buku : "<<i;
         b = daftar_buku[i];
         tampil_buku(b);
-        cout<<endl;
+        cout<<endl<<endl;
     }
+    main_menu();
 }
--- buku.cpp
+++ buku.cpp
@@ -1,13 +1,13 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 40-02
+    KELOMPOK  : 2
+    NAMA(NIM) : [Muh. Hatta Eka Putra][1301160444] , [Rifki Mifathur Sutomo][1301164225] , [Fahmi Hidayat][1301164312] , [Elsya Putri Sutomo][1301160201]
 **/
-
 
 #include "buku.h"
 
-buku create_buku(string judul, string pengarang) {
+buku create_buku(string judul, string pengarang)
+{
     /**
     * fungsi mengeset buku b dengan judul, dan pengarang
     * dan mengembalikan hasilnya
@@ -16,13 +16,17 @@ buku create_buku(string judul, string pengarang) {
     //=================================================
     // YOUR CODE STARTS HERE
 
+    b.judul = judul;
+    b.pengarang = pengarang;
+    b.status_pinjam = false;
 
     // YOUR CODE ENDS HERE
     //=================================================
     return b;
 }
 
-void pinjam_buku(buku &b, int tgl, int bln, int thn) {
+void pinjam_buku(buku &b, int tgl, int bln, int thn)
+{
     /**
     * fungsi mengeset tanggal pinjam dari buku b dengan tgl, bln, thn,
     * dan mengeset status pinjam menjadi true
@@ -31,12 +35,33 @@ void pinjam_buku(buku &b, int tgl, int bln, int thn) {
     //=================================================
     // YOUR CODE STARTS HERE
 
+    if (cek_ketersediaan(b) == false)
+    {
+        cout<<" > Mohon Maaf, Buku Sedang Dipinjam"<<endl<<endl;
+    }
+    else
+    {
+        b.tgl = tgl;
+        b.bln = bln;
+        b.thn = thn;
+
+        if (cek_tanggal(create_tanggal(tgl,bln,thn)))
+        {
+            b.status_pinjam = true;
+        }
+        else
+        {
+            cout<<" > Mohon Maaf, Tanggal yang Andah Masukkan Salah!"<<endl<<endl;
+            b.status_pinjam = false;
+        }
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
 }
 
-int kembalikan_buku(buku &b, int tgl, int bln, int thn) {
+int kembalikan_buku(buku &b, int tgl, int bln, int thn)
+{
     /**
     * fungsi mengeset status pinjam menjadi false
     * fungsi memanggil fungsi create_tanggal untuk membuat tanggal kembali
@@ -44,8 +69,20 @@ int kembalikan_buku(buku &b, int tgl, int bln, int thn) {
     * dengan menggunakan fungsi selisih_hari
     **/
     int selisih;
+    tanggal tgl1;
+    tanggal tgl2;
     //=================================================
     // YOUR CODE STARTS HERE
+
+
+    tgl1.dd = b.tgl;
+    tgl1.mm = b.bln;
+    tgl1.yy = b.thn;
+    tgl2 = create_tanggal(tgl,bln,thn);
+    selisih = selisih_hari(tgl1,tgl2);
+    if (selisih != -1){
+        b.status_pinjam = false;
+    }
 
 
     // YOUR CODE ENDS HERE
@@ -54,7 +91,8 @@ int kembalikan_buku(buku &b, int tgl, int bln, int thn) {
 }
 
 
-void tampil_buku(buku b) {
+void tampil_buku(buku b)
+{
     /**
     * fungsi menampilkan judul, pengarang, dan status dipinjam dari buku b
     * contoh output:
@@ -66,6 +104,39 @@ void tampil_buku(buku b) {
     // YOUR CODE STARTS HERE
 
 
+    cout <<endl;
+    cout <<"  > Judul Buku : "<<b.judul<<endl;
+    cout <<"  > Nama Pengarang : "<<b.pengarang<<endl;
+    cout <<"  > Status : ";
+    if (b.status_pinjam)
+    {
+        cout<<"Sudah Dipinjam"<<endl;
+        cout<<"  > Dipinjam Tanggal : ";
+        tampil_tanggal(create_tanggal(b.tgl,b.bln,b.thn));
+    }
+    else
+    {
+        cout<<"Belum Dipinjam";
+    }
+
     // YOUR CODE ENDS HERE
     //=================================================
+}
+
+//Fungsi Tambahan Agar Tidak Bisa Meminjam Buku Yang Sedang Dipinjam
+
+bool cek_ketersediaan(buku b)
+{
+    bool cek;
+
+    if (b.status_pinjam == true)
+    {
+        cek = false;
+    }
+    else
+    {
+        cek = true;
+    }
+
+    return cek;
 }
