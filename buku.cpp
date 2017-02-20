45b820b37f20d3efe730fb47f22c5175bb36bef8
diahevyka <diah.hevyka@gmail.com>
2017-02-20 23:25:57 +0700

    12

--- buku.cpp
+++ buku.cpp
@@ -1,9 +1,8 @@
 /**
-    KELAS     :
-    KELOMPOK  :
-    NAMA(NIM) : [nama][nim] , [nama][nim] , [nama][nim] , [nama][nim]
+    KELAS     : IF 40-02
+    KELOMPOK  : Kelompok 4
+    NAMA(NIM) : [Joel Andrew][1301164165Lucky Olivera][1301164055] , [Rangga Dwi A][1301164030] , [Diah Hevyka M][1301164336]
 **/
-
 
 #include "buku.h"
 
