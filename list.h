afb24cb2b882ff0f4219965a6f283902fab11de8
Faishal Raihan <faishalraihan73@gmail.com>
2019-02-08 18:56:04 +0700

    1301184163
    TASK 2 DONE
    FAISHAL RAIHANUR RASYID
    1301184163

--- list.h
+++ list.h
@@ -31,14 +31,15 @@ typedef struct elmlist *address;
 struct elmlist{
     //------------- your code here -----------
 
-
+infotype info;
+address next;
     //----------------------------------------
 };
 
 struct List{
     //------------- your code here -----------
-
-    //----------------------------------------
+address first;
+   //----------------------------------------
 };
 
 
--- operation.cpp
+++ operation.cpp
@@ -2,7 +2,8 @@
 #include "operation.h"
 
 
-void insert_sorted(List &L, infotype x) {
+void insert_sorted(List &L, infotype x)
+{
     /**
     * IS : List may be empty
     * PR : insert an infotype x into an already sorted List L
@@ -12,9 +13,31 @@ void insert_sorted(List &L, infotype x) {
     * FS : elements in List L sorted in ascending order, x is inside List L
     */
 
-    //-------------your code here-------------
-    cout<<"your code here"<<endl;
-
+    address P = allocate(x);
+    address Q = first(L);
+    if (Q == NULL)
+    {
+        insertFirst(L,P);
+    }
+    else if (findElm(L,x) == NULL)
+    {
+        if (info(P) < info(Q))
+        {
+            insertFirst(L,P);
+        }
+        else if (next(Q) == NULL)
+        {
+            insertLast(L,P);
+        }
+        else
+        {
+            while (next(Q) != NULL && info(Q) < info(P))
+            {
+                Q = next(Q);
+            }
+            insertAfter(L,Q,P);
+        }
+    }
 
     //----------------------------------------
 }
