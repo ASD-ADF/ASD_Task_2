d41f8ff97d9d18be593701a7fc468b5c3a033033
rlzmtnck <54921947+rlzmtnck@users.noreply.github.com>
2020-02-08 19:36:03 +0700

    DONE
    DONE

--- operation.cpp
+++ operation.cpp
@@ -12,9 +12,23 @@ void insert_sorted(List &L, infotype x) {
     * FS : elements in List L sorted in ascending order, x is inside List L
     */
 
-    //-------------your code here-------------
-    cout<<"your code here"<<endl;
 
+    address Q,P;
+    if((first(L)==NULL) || (x<info(first(L)))) {
+        P = allocate(x);
+        insertFirst(L,P);
+    } else if(findElm(L,x)==NULL){
+        Q = first(L);
+        while((next(Q)!=NULL) && (x>info(next(Q)))){
+            Q = next(Q);
+            if(next(Q)==NULL){
+                P = allocate(x);
+                insertLast(L,P);
+            } else if(info(P)!=info(Q)){
+                P = allocate(x);
+                insertAfter(L,P,Q);
+            }
+        }
 
-    //----------------------------------------
+    }
 }
